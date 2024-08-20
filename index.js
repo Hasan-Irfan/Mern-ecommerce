b1dbcac002f1725c2301d3c501234d3d93c93742
Hasan-Irfan <riyankam82@gmail.com>
2024-08-20 10:43:50 -0700

    m
    m

--- index.js
+++ index.js
@@ -6,9 +6,11 @@ dotenv.config({
   path: '.env'
 });
 
+const port = 4000;
+
 connectDB().then(
-  app.listen(process.env.PORT || 8000 , () => {
-    console.log(`Example app listening on port ${process.env.PORT || 8000}`)
+  app.listen(process.env.PORT || port , () => {
+    console.log(`Example app listening on port ${process.env.PORT || port}`)
   })
 ).catch((error)=>{
   console.error("MONGO Connection ERROR", error);
